0339145dda038f128b3faafd08096b3f7624fb06
Adrian L Lange <git@p3lim.net>
2023-09-17 17:05:20 +0200

    :sparkles:

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM docker.io/library/alpine:3.18
+
+RUN <<EOM
+    apk add --no-cache bind bind-tools gettext
+    chown named:named /etc/bind /var/bind
+EOM
+
+COPY --chown=named:named --chmod=0755 entrypoint.sh /
+COPY --chown=named:named --chmod=0644 templates /templates
+
+EXPOSE 53/udp
+EXPOSE 8053/tcp
+
+USER named
+
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ "z$BIND_KEY_ALG" = 'z' ]; then
+  export BIND_KEY_ALG='hmac-sha256'
+fi
+
+if [ "$1" = 'tsig' ]; then
+  # generate tsig key and expose variables
+  export SECRET_KEY="$(/usr/sbin/tsig-keygen -a $BIND_KEY_ALG externaldns-key | /usr/bin/awk -F'"' '/secret/{print $2}' | /bin/base64)"
+  export SECRET_NAME="${1:-bind-tsig}"
+  export SECRET_NAMESPACE="$2"
+
+  # render manifest to stdout
+  /usr/bin/envsubst < /templates/secret.yaml | /bin/sed '/namespace: $/d'
+
+  exit 0
+fi
+
+# check if key is defined
+if [ "z$BIND_KEY" = 'z' ]; then
+  echo 'key not defined'
+  exit 1
+fi
+
+# check if zone is defined
+if [ "z$BIND_ZONE" = 'z' ]; then
+  echo 'zone not defined'
+  exit 1
+fi
+
+# define substitution values
+export ZONE_SERIAL="$(/bin/date +%s)"
+export ZONE_NS_IP="$(/bin/hostname -i)"
+
+# render config files
+/usr/bin/envsubst < /templates/named.conf > /etc/bind/named.conf
+/usr/bin/envsubst < /templates/zone.db > /etc/bind/zone.db
+
+# validate config
+named-checkconf /etc/bind/named.conf
+
+# dump version info and run named
+/usr/sbin/named -v
+exec /usr/sbin/named -4 -f -c /etc/bind/named.conf
